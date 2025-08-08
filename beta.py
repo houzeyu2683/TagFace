2a6d43db782aad16e821d822df7f21a379dcbd96
houzeyu2683 <houzeyu2683@gmail>
2025-08-08 12:00:38 +0800

    add

--- beta.py
+++ beta.py
@@ -1,345 +1,83 @@
-
-
 import yt_dlp
+import pandas
+import os
+import shutil
+import multiprocessing
 
-# 替換為你的播放清單 URL
-URL = 'https://www.youtube.com/watch?v=B3-sjPhiM2U'
+class Video:
 
-# SHORT_URL = 'https://www.youtube.com/shorts/zwMEhBq4kYM'
+    def __init__(self, link: str, folder: str) -> None:
+        self.link = link
+        self.folder = folder
+        return
 
-# PLAYLIST_URL = 'https://www.youtube.com/playlist?list=PL9mUJWHev0KllR1T1qgBuoj0_NtFHzBuf'
+    def searchCatalog(self, query: None|str) -> bool:
+        option = {
+            'quiet': True,
+            'extract_flat': True,
+            'force_generic_extractor': True,
+            'no_warnings': True
+        }
+        with yt_dlp.YoutubeDL(option) as session:
+            response = session.extract_info(link, download=False)
+            information = response['entries']
+            title = [item['title'] for item in information]
+            name = [item['id'] for item in information]
+            duration = [item['duration'] for item in information]
+            pass
+        host = 'https://www.youtube.com/watch?v='
+        link = [f'{host}{item}' for item in name]
+        table = pandas.DataFrame({
+            "name": name, 
+            'title': title, 
+            'duration': duration, 
+            'link': link
+        })
+        table = table.dropna().reset_index(drop=True)
+        if(query!=None): 
+            table = table.query(query, engine='python')
+            table = table.reset_index(drop=True)
+            pass
+        self.catalog = table
+        return(True)
 
-# 設定 yt-dlp 選項
-ydl_opts = {
-    'format': 'bestvideo+bestaudio',
-    'outtmpl': './downloads/%(id)s.%(ext)s',
-    'merge_output_format': 'mp4',
+    def downloadFile(self, link: str) -> bool:
+        path = os.path.join(self.folder, '%(id)s.%(ext)s')
+        option = {
+            'format': 'bestvideo+bestaudio',
+            'outtmpl': path,
+            'merge_output_format': 'mp4',
+            'postprocessor_args': {
+                'ffmpeg': [
+                    '-r', '25', 
+                    '-ar', '16000',
+                    '-crf', '23'
+                ]
+            },
+            'noplaylist': False,
+            'quiet': False,
+            'no_warnings': False,
+            'verbose': True,
+        }
+        with yt_dlp.YoutubeDL(option) as command:
+            command.download(link)
+            pass
+        command.close()
+        return(True)
 
-    'postprocessor_args': {
-        'ffmpeg': [
-            '-r', '25',          # 設定 25 fps
-            '-ar', '16000',      # 設定音訊為 16kHz
-            '-crf', '23',
-            # '-preset', 'fast'
-        ]
-    },
+    def saveCatalog(self) -> bool:
+        checkpoint = os.path.join(self.folder)
+        _ = shutil.rmtree(checkpoint, ignore_errors=True)
+        os.makedirs(checkpoint, exist_ok=True)
+        path = os.path.join(checkpoint, 'catalog.csv')
+        if('table'):
+            self.catalog.to_csv(path, index=False)
+            pass
+        loop = self.catalog['link'].tolist()
+        with multiprocessing.Pool(processes=4) as pool:
+            pool.map(self.downloadFile, loop)
+            pass
+        pool.close()
+        return(True)
 
-    'noplaylist': False,
-    'quiet': False,
-    'no_warnings': False,
-    'verbose': True,
-}
-
-
-
-
-# 執行下載
-with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(URL)
-
-
-# import skimage.metrics
-# import moviepy
-# import PIL.Image
-# import numpy
-# import torch
-# import os
-# import skimage
-# import json
-# import numpy
-# import face_alignment
-# import torchvision
-# import itertools
-
-# class Metric:
-
-#     def __init__(self, method: str) -> None:
-#         self.method = method
-#         return
-
-#     def getScore(
-#         self,
-#         prediction: numpy.ndarray, 
-#         truth: numpy.ndarray
-#     ) -> float:
-#         if(self.method=='IoU'):
-#             score = torchvision.ops.box_iou(
-#                 torch.tensor(prediction)[None],
-#                 torch.tensor(truth)[None]
-#             )
-#             score = score.item()
-#             pass
-#         elif(self.method=='Similarity'):
-#             score = skimage.metrics.structural_similarity(
-#                 prediction,
-#                 truth,
-#                 data_range=255.0
-#             )
-#             score = numpy.array(score).item()
-#             pass
-#         score = round(score, 3)
-#         return(score)
-
-#     pass
-
-# class Shot:
-
-#     def __init__(
-#         self, 
-#         frame: numpy.ndarray,
-#         box: numpy.ndarray,
-#         landmark: numpy.ndarray
-#     ) -> None:
-#         self.frame = frame
-#         self.box = box
-#         self.landmark = landmark
-#         return
-
-#     def getArea(self, size: tuple, color: bool) -> numpy.ndarray:
-#         image = PIL.Image.fromarray(self.frame)
-#         image = image.crop(self.box)
-#         if(not color): image = image.convert("L")
-#         image = image.resize(size)
-#         area = numpy.array(image)
-#         return(area)
-
-#     def getSize(self) -> tuple:
-#         height, width, _ = self.frame.shape
-#         size = (height, width)
-#         return(size)
-
-#     pass
-
-# class Trajectory:
-
-#     def __init__(self, size: tuple, fragment: list, timestep: list) -> None:
-#         self.size = size
-#         self.fragment = fragment
-#         self.timestep = timestep
-#         return
-
-#     def getContinuity(self, shot: Shot) -> bool:
-#         memory = self.fragment[-1]
-#         assert isinstance(memory, Shot)
-#         score = Metric('IoU').getScore(memory.box, shot.box)
-#         if(score<0.8): return(False)
-#         size = (64, 64)
-#         truth = memory.getArea(size, color=False)
-#         prediction = shot.getArea(size, color=False)
-#         score = Metric('Similarity').getScore(prediction, truth)
-#         if(score<0.8): return(False)
-#         return(True)
-
-#     def increaseFragment(self, shot: Shot) -> bool:
-#         self.fragment = self.fragment + [shot]
-#         return(True)
-
-#     def increaseTimestep(self, second: float) -> bool:
-#         self.timestep = self.timestep + [second]
-#         return(True)
-
-#     def getRegion(self) -> numpy.ndarray:
-#         # 統一所有box的
-#         # size = None
-#         box = []
-#         for shot in self.fragment:
-#             assert isinstance(shot, Shot)
-#             # size += [shot.getSize()]
-#             # if(size==None): size = shot.getSize()
-#             box += [shot.box]
-#             continue
-#         # height, width = size
-#         box = numpy.stack(box)
-#         origin = (
-#             box[:,0].min(), # left width
-#             box[:,1].min(), # left height
-#             box[:,2].max(), # right width
-#             box[:,3].max() # right height
-#         )
-#         delta = [
-#             (origin[2] - origin[0]) * 0.1, # width delta
-#             (origin[3] - origin[1]) * 0.1 # height delta
-#         ]
-#         height, width = self.size
-#         region = (
-#             max(origin[0]-delta[0], 0),
-#             max(origin[1]-delta[1], 0),
-#             min(origin[2]+delta[0], width),
-#             min(origin[3]+delta[1], height)
-#         )        
-#         return(region)
-
-#     pass
-
-# class Agent:
-
-#     def __init__(self, path: str) -> None:
-#         self.path = path
-#         return
-
-#     def makeDetection(self, frame: numpy.ndarray) -> bool:
-#         response = self.model.get_landmarks(
-#             frame, 
-#             return_bboxes=True, 
-#             return_landmark_score=True
-#         )
-#         detection = []
-#         for landmark, likelihood, prediction in zip(*response):
-#             count = numpy.array(sum(likelihood<0.7)).item() # likelihood (landmark)
-#             if(count>0): continue
-#             box = numpy.array(prediction[:-1]).astype(int)
-#             probability = round(float(prediction[-1]), 3)
-#             if(probability<0.8): continue
-#             shot = Shot(frame, box, landmark)
-#             detection += [shot]
-#             continue
-#         self.detection = detection
-#         return(True)
-
-#     def readVideo(self) -> bool:
-#         video = moviepy.VideoFileClip(self.path)
-#         assert (video.fps == 25) and (video.duration >= 1)
-#         self.video = video
-#         return(True)
-
-#     def captureSequence(self) -> bool:
-#         delta = 1 / self.video.fps
-#         sequence = {}
-#         for index, frame in enumerate(agent.video.iter_frames(), start=0):
-#             second = index / self.video.fps
-#             unit = round(second + delta, 3)
-#             print(f'Analyze [{second}, {unit}) Interval', end='\r')
-#             self.makeDetection(frame)
-#             if(self.queue!=[] and self.detection==[]): 
-#                 # 所有的 queue 都打包成一個完整的片段
-#                 for trajectory in self.queue:
-#                     item = {self.total: trajectory}
-#                     sequence.update(item)
-#                     self.total += 1
-#                     continue
-#                 _, _ = index, frame
-#                 self.queue==[]
-#                 continue
-#             if(self.queue!=[] and self.detection!=[]):
-#                 # 查詢可以合併的 trajectory 跟 shot
-#                 rule = []
-#                 grid = list(
-#                     itertools.product(
-#                         range(len(self.queue)), 
-#                         range(len(self.detection))
-#                     )
-#                 )
-#                 for cell in grid:
-#                     trajectory = self.queue[cell[0]]
-#                     shot = self.detection[cell[1]]
-#                     assert isinstance(trajectory, Trajectory)
-#                     assert isinstance(shot, Shot)
-#                     continuity = trajectory.getContinuity(shot)
-#                     if(continuity): rule += [cell]
-#                     continue
-#                 rule = numpy.array(rule)
-#                 summary = numpy.unique(rule, axis=0)
-#                 if(summary.size==0): summary = numpy.array([(-1, -1)])
-#                 # 合併批配的 trajectory 跟 shot
-#                 for cell in summary:
-#                     if(cell[0]==-1 and cell[1]==-1): continue
-#                     trajectory = self.queue[cell[0]]
-#                     shot = self.detection[cell[1]]
-#                     assert isinstance(trajectory, Trajectory)
-#                     assert isinstance(shot, Shot)
-#                     trajectory.increaseFragment(shot)
-#                     trajectory.increaseTimestep(second)
-#                     continue
-#                 # 未批配的 trajectory 取出存起來
-#                 acceptance = []
-#                 for anchor, trajectory in enumerate(self.queue):
-#                     if(anchor in summary[:, 0]): 
-#                         acceptance += [trajectory]
-#                         continue
-#                     item = {self.total: trajectory}
-#                     sequence.update(item)
-#                     self.total += 1
-#                     continue
-#                 self.queue = acceptance
-#                 # 未批配的 shot 變成新的 Trajectory 並且新增至 queue
-#                 for anchor, shot in enumerate(self.detection):
-#                     assert isinstance(shot, Shot)
-#                     if(anchor in summary[:, 1]): continue
-#                     trajectory = Trajectory(
-#                         size=shot.getSize(), 
-#                         fragment=[shot], 
-#                         timestep=[second]
-#                     )
-#                     self.queue += [trajectory]
-#                     continue
-#                 _, _ = anchor, frame
-#                 continue
-#             if(self.queue==[] and self.detection!=[]):
-#                 for anchor, shot in enumerate(self.detection):
-#                     assert isinstance(shot, Shot)
-#                     trajectory = Trajectory(
-#                         size=shot.getSize(), 
-#                         fragment=[shot], 
-#                         timestep=[second]
-#                     )
-#                     self.queue += [trajectory]
-#                     continue
-#                 _, _ = index, frame
-#                 continue
-#             _, _ = anchor, frame
-#             continue
-#         self.sequence = sequence
-#         return(True)
-
-#     def filterSequence(self) -> bool:
-#         # 挑出嘴巴有在動的
-#         # 或是聲音與臉部批配的
-#         sequence = {}
-#         for index in self.sequence:
-#             trajectory = self.sequence[index]
-#             assert isinstance(trajectory, Trajectory)
-#             second = trajectory.timestep[-1] - trajectory.timestep[0]
-#             if(second<=1): continue
-#             region = trajectory.getRegion()
-#             width = (region[2] - region[0])
-#             height = (region[3] - region[1])
-#             if(width<128 or height<128): continue
-#             sequence.update({index: trajectory})
-#             continue
-#         self.sequence = sequence
-#         return(True)
-
-#     def saveSequence(self, checkpoint: str) -> bool:
-#         for index in self.sequence:
-#             path = os.path.join(checkpoint, f'{index}.mp4')
-#             os.makedirs(os.path.dirname(path), exist_ok=True)
-#             trajectory = self.sequence[index]
-#             assert isinstance(trajectory, Trajectory)
-#             region = trajectory.getRegion()
-#             interval = trajectory.timestep[0], trajectory.timestep[-1]
-#             segment = self.video.subclipped(*interval)
-#             assert isinstance(segment, moviepy.VideoFileClip)
-#             segment = segment.cropped(*region)
-#             segment.write_videofile(
-#                 path, codec="libx264", audio_codec="aac", logger=None
-#             )
-#             continue
-#         return(True)
-
-#     def launchJob(self, checkpoint: str) -> bool:
-#         self.readVideo()
-#         self.captureSequence()
-#         self.filterSequence()
-#         self.saveSequence(checkpoint)
-#         return(True)
-
-#     total = 0
-#     queue = []
-#     model = face_alignment.FaceAlignment(
-#         face_alignment.LandmarksType.TWO_D, 
-#         device='cuda'
-#     )    
-#     pass
-
-# agent = Agent(path='./video/_V1NYVMWkiE_30sec.mp4')
-# agent.launchJob(checkpoint='checkpoint')
+    pass
